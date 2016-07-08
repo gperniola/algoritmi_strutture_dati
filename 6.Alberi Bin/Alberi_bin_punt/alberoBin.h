669e35fab0babfd744da4a92e283d8ed23df2ec6
gperniola <g.perniola22@gmail.com>
2016-07-08 16:41:26 +0200

    added albero bin structure

---
+++ 6.Alberi Bin/Alberi_bin_punt/alberoBin.h
@@ -0,0 +1,115 @@
+#ifndef ALBEROBIN_H
+#define ALBEROBIN_H
+
+#include <iostream>
+using namespace std;
+
+
+template <class T, class N>
+class AlberoBin {
+public:
+
+    // tipi
+    typedef T tipoElem;
+    typedef N nodo;
+
+    virtual void creaAlberoBin() = 0;
+    virtual bool alberoBinVuoto() = 0;
+    virtual nodo binRadice() = 0;
+    virtual nodo binPadre(nodo) = 0;
+    virtual nodo binFiglioSin(nodo) = 0;
+    virtual nodo binFiglioDes(nodo) = 0;
+    virtual bool sinVuoto(nodo) = 0;
+    virtual bool desVuoto(nodo) = 0;
+
+    virtual void scriviNodo(tipoElem, nodo) = 0;
+    virtual tipoElem leggiNodo(nodo) = 0;
+
+    virtual void costrBinAlbero(AlberoBin<T,N>, AlberoBin<T,N>) = 0
+    virtual void cancsottoAlbero(nodo) = 0;
+
+    virtual void insBinRadice(nodo) = 0;
+    virtual void insFiglioSin(nodo) = 0;
+    virtual void insFiglioDes(nodo) = 0;
+
+    // funzioni di servizio da implementare qui
+    //virtual void preVisita(nodo);
+    //virtual void inVisita(nodo);
+    //virtual void postVisita(nodo);
+
+    virtual void print() const;
+
+private:
+    virtual void printSubTree(const Nodo) const;
+
+};
+
+
+
+
+template <class T, class N>
+void Bin_tree<T,N>::print() const{
+	if (!empty())
+		printSubTree(root());
+	else
+		std::cout << "[]" << std::endl;
+	std::cout << std::endl;
+}
+
+template <class T, class N>
+void Bin_tree<T,N>::printSubTree(const Nodo n) const{
+	std::cout << "[" << read(n) << ", ";
+	if (!sx_empty(n))
+		printSubTree(sx(n));
+	else std::cout << "NIL";
+	std::cout << ", ";
+	if (!dx_empty(n))
+		printSubTree(dx(n));
+	else std::cout << "NIL";
+	std::cout << " ]";
+}
+
+template<class T, class N>
+std::ostream& operator<<(std::ostream& out, const Bin_tree<T, N>& t) {
+    t.print();
+    return out;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ 6.Alberi Bin/Alberi_bin_punt/cell.h
@@ -0,0 +1,115 @@
+#ifndef CELL_H_INCLUDED
+#define CELL_H_INCLUDED
+
+template <class T>
+class Cell{
+
+public:
+    typedef T value_type;
+
+    /*****constructors*****/
+    Cell();
+    //Cell(const value_type &);
+    //Cell(value_type &, Cell<T>* p, Cell<T>* n);
+
+    /*****destructor*****/
+    ~Cell();
+
+    /*****getters*****/
+    value_type get_element() const;
+    Cell<T>* get_figlioSin() const;
+    Cell<T>* get_figlioDes() const;
+    Cell<T>* get_padre() const;
+
+    /*****setters*****/
+    void set_element(value_type);
+    void set_figlioSin(Cell<T>*);
+    void set_figlioDes(Cell<T>*);
+    void set_padre(Cell<T>*);
+
+    /*****operators overload*****/
+    Cell<T>& operator = (const Cell<T> &);
+
+private:
+    value_type element;
+    Cell<T>* figlioSin;
+    Cell<T>* figlioDes;
+    Cell<T>* padre;
+};
+
+/***********implementation*************/
+
+template <class T>
+Cell<T>::Cell(){
+    this->figlioSin = nullptr;
+    this->figlioDes = nullptr;
+    this->padre = nullptr;
+    }
+
+/*template <class T>
+Cell<T>::Cell(const value_type &value){
+    this->element = value;
+    Cell();
+    }*/
+
+/*template <class T>
+Cell<T>::Cell(value_type &value, Cell<T>* p, Cell<T>* n){
+    this->element = value;
+    this->next = n;
+    this->previous = p;
+    }*/
+
+template <class T>
+Cell<T>::~Cell(){}
+
+template <class T>
+typename Cell<T>::value_type Cell<T>::get_element() const{
+    return this->element;
+    }
+
+template <class T>
+Cell<T>* Cell<T>::get_figlioSin() const{
+    return this->figlioSin;
+    }
+
+template <class T>
+Cell<T>* Cell<T>::get_figlioDes() const{
+    return this->figlioDes;
+    }
+
+template <class T>
+Cell<T>* Cell<T>::get_padre() const{
+    return this->padre;
+    }
+
+template <class T>
+void Cell<T>::set_element(value_type value){
+    this->element = value;
+}
+
+template <class T>
+void Cell<T>::set_figlioSin(Cell<T>* p){
+    this->figlioSin = p;
+}
+
+template <class T>
+void Cell<T>::set_figlioDes(Cell<T>* p){
+    this->figlioDes = p;
+}
+
+template <class T>
+void Cell<T>::set_padre(Cell<T>* p){
+    this->padre = p;
+}
+
+/*****operators*****/
+
+template <class T>
+Cell<T>& Cell<T>::operator = (const Cell<T> &c){
+    this->element = c.get_element();
+    this->figlioSin = c.get_figlioSin();
+    this->figlioDes = c.get_figlioDes();
+    this->padre = c.get_padre();
+}
+
+#endif // CELL_H_INCLUDED
