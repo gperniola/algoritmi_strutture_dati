05b69846551eb39eedd3fd43f3bdac2dfcec8fc3
gperniola <g.perniola22@gmail.com>
2016-07-15 10:42:40 +0200

    added alberi n

---
+++ 7.Alberi n/alberi_n_punt/alberoN.h
@@ -0,0 +1,138 @@
+#ifndef ALBERON_H_
+#define ALBERON_H_
+
+#include <iostream>
+using namespace std;
+
+template <class T, class N>
+class AlberoN{
+public:
+
+    // tipi
+    typedef T tipoElem;
+    typedef N nodo;
+
+    virtual void creaAlbero() = 0;
+    virtual bool alberoVuoto() const = 0;
+    virtual nodo radice()const = 0;
+    virtual nodo padre(nodo)const = 0;
+    virtual bool foglia(nodo)const = 0;
+    virtual nodo primoFiglio(nodo)const = 0;
+    virtual nodo succFratello(nodo)const = 0;
+
+    virtual void insRadice() = 0;
+    virtual void insRadice(nodo) = 0;
+
+    virtual void insPrimoSottoalbero(nodo, AlberoN<T,N>&) = 0;
+    virtual void insSottoalbero(nodo, AlberoN<T,N>&) = 0;
+    virtual void cancSottoalbero(nodo) = 0;
+
+    virtual void scriviNodo(tipoElem, nodo) = 0;
+    virtual tipoElem leggiNodo(nodo) const = 0;
+
+ // funzioni di servizio da implementare qui
+    virtual void preVisita(nodo);
+    virtual void inVisita(nodo, int);
+    virtual void postVisita(nodo);
+
+    virtual void print() const;
+
+    virtual int maxLivello() const;
+    virtual int dimAlbero(nodo) const;
+
+private:
+    virtual void printSubTree(const nodo) const;
+    virtual int calcolaMaxLivello(nodo) const;
+
+};
+
+template <class T, class N>
+void AlberoN<T,N>::preVisita(nodo n){
+    if (!alberoVuoto()){
+        cout << leggiNodo(n) << ", ";
+        if(!foglia(n)){
+            nodo m = primoFiglio(n);
+            preVisita(m);
+            while(!ultimoFratello(m)){
+                m = succFratello(m);
+                preVisita(m);
+            }
+        }
+    }
+}
+
+template <class T, class N>
+void AlberoN<T,N>::postVisita(nodo n){
+    if (!alberoVuoto()){
+        if(!foglia(n)){
+            nodo m = primoFiglio(n);
+            postVisita(m);
+            while(!ultimoFratello(m)){
+                m = succFratello(m);
+                postVisita(m);
+            }
+        }
+    cout << leggiNodo(n) << ", ";
+    }
+}
+
+template <class T, class N>
+void AlberoN<T,N>::inVisita(nodo n, int i){
+    if (!alberoVuoto()){
+        if(!foglia(n)){
+            if( i < 1) i = 1;
+            nodo m = primoFiglio(n);
+            inVisita(m, i);
+
+            int k = 2;
+            while (k <= i && !ultimoFratello(m)){
+                m = succFratello(m);
+                inVisita(m, i);
+                k++;
+            }
+            cout << leggiNodo(n) << ", ";
+            while(!ultimoFratello(m)){
+                m = succFratello(m);
+                inVisita(m, i);
+            }
+        }
+        else
+            cout << leggiNodo(n) << ", ";
+    }
+}
+
+template <class T, class N>
+void AlberoN<T,N>::print() const{
+	if (!this->alberoVuoto())
+		printSubTree(radice());
+	else
+		std::cout << "[]" << std::endl;
+	std::cout << std::endl;
+}
+
+template <class T, class N>
+void AlberoN<T,N>::printSubTree(const nodo n) const{
+	std::cout << "[" << leggiNodo(n);
+	if(!foglia(n)){
+        cout << ", ";
+        nodo m = primoFiglio(n);
+        printSubTree(m);
+        while(!ultimoFratello(m)){
+            cout << ", ";
+            m = succFratello(m);
+            printSubTree(m);
+        }
+    }
+    std::cout << " ]";
+}
+
+
+
+
+
+
+
+
+
+
+#endif
