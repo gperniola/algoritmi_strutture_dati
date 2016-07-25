f960ea14b8eaa4bff73f05bf2ee40a92bec15730
gperniola <g.perniola22@gmail.com>
2016-07-25 14:58:29 +0200

    fixed inserisci function

--- 8.Code priorita/CodeP_alberi/main.cpp
+++ 8.Code priorita/CodeP_alberi/main.cpp
@@ -6,15 +6,14 @@ using namespace std;
 int main()
 {
     CodaPAlberoBin<int> coda;
-    coda.inserisci(1);
-    coda.inserisci(2);
     coda.inserisci(3);
+    coda.inserisci(2);
     coda.inserisci(4);
     coda.inserisci(5);
-    coda.inserisci(6);
+    coda.inserisci(3);
     coda.inserisci(7);
-    coda.inserisci(8);
-    coda.inserisci(9);
+    coda.inserisci(6);
+    coda.inserisci(1);
     coda.print();
     cout << "Hello world!" << endl;
     return 0;
