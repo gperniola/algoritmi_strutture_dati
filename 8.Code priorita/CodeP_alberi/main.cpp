64e62f58ea081296d8c1c3555735ecf3f57e9a72
gperniola <g.perniola22@gmail.com>
2016-07-25 12:46:07 +0200

    completed part 1 of insert function

--- 8.Code priorita/CodeP_alberi/main.cpp
+++ 8.Code priorita/CodeP_alberi/main.cpp
@@ -8,6 +8,13 @@ int main()
     CodaPAlberoBin<int> coda;
     coda.inserisci(1);
     coda.inserisci(2);
+    coda.inserisci(3);
+    coda.inserisci(4);
+    coda.inserisci(5);
+    coda.inserisci(6);
+    coda.inserisci(7);
+    coda.inserisci(8);
+    coda.inserisci(9);
     coda.print();
     cout << "Hello world!" << endl;
     return 0;
